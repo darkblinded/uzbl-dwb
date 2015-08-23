5045d283d59bc564d784f9f5decb2d2ecaaba44d
Leonard Frank <dev@leonardfrank.de>
2015-08-23 18:50:48 +0200

    Added noscript.sh to disable javascipt by default except for sites in ns-whitelist

---
+++ data/scripts/noscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+WHITELIST=$XDG_DATA_HOME/uzbl/ns_whitelist
+ 
+while read -r line; do
+    if [[ "$UZBL_URI" =~ "$line" ]]; then
+        echo 'set enable_scripts = 1' > "$UZBL_FIFO"
+        break
+    else
+        echo 'set enable_scripts = 0' > "$UZBL_FIFO"
+    fi
+done < "$WHITELIST"
