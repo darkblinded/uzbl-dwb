6c983c7c2b49c66374ee48067881f159e7b4f60c
Leonard Frank <dev@leonardfrank.de>
2015-08-26 09:41:47 +0200

    Added searchprefix for archwiki, chefkoch and nzbindex

--- data/scripts/openstring.js
+++ data/scripts/openstring.js
@@ -9,6 +9,14 @@ function getSearchUrl (o) {
         //Define custom search prefixes
         if (searchPrefix("g")) {
             return 'http://www.google.com/search?q=' + o.substring(2);
+        } else if (searchPrefix("aw")) {
+            return 'https://wiki.archlinux.org/index.php/Special:Search?fulltext=Search&search=%s' + o.substring(3)
+        } else if (searchPrefix("ck")) {
+            return 'http://www.chefkoch.de/rs/s0/' + o.substring(3).replace(" ","+") + "/Rezepte.html"
+        } else if (searchPrefix("nzb")) {
+            return 'HTTP://nzbindex.com/search/?sort=size&minsize=100&q=' + o.substring(4)
+        //} else if (searchPrefix("XXX")) {
+        //    return '<SEARCH URL>' + o.substring(2)
         } else {
             return 'http://www.google.com/search?q=' + o;
         }
